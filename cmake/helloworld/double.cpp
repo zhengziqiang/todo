874afd0d006a5929392a5b813851b4b8f2bc5e94
zhengziqiang <1174986943@qq.com>
2016-10-23 10:38:57 +0800

    cmake demo

---
+++ cmake/helloworld/double.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: 多个文件.cpp
+    > Author: zzq
+    > Mail: zhengziqiang1@gmail.com 
+    > Created Time: 2016年08月12日 星期五 08时00分34秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int fun(){
+	cout<<"多个文件"<<endl;
+	return 0;
+}
---
+++ cmake/helloworld/helloworld.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: helloworld.cpp
+    > Author: zzq
+    > Mail: zhengziqiang1@gmail.com 
+    > Created Time: 2016年08月12日 星期五 07时47分38秒
+ ************************************************************************/
+
+#include<iostream>
+#include"double.cpp"
+using namespace std;
+int main(){
+	cout<<"hello world"<<endl;
+	fun();
+	return 0;
+}
